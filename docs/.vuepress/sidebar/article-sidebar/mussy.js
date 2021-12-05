19d1bd778cbcef0863e883ceae81fb539502ca2f
AlanNgaiJX <634620668@qq.com>
2021-12-05 11:57:27 +0800

    增加杂记: 新增202112月杂记

---
+++ docs/.vuepress/sidebar/article-sidebar/mussy.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: your name
+ * @Date: 2021-12-05 11:54:30
+ * @LastEditTime: 2021-12-05 11:54:32
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /alan-blog/docs/.vuepress/sidebar/article-sidebar/mussy.js
+ */
+module.exports = {
+  title: "mussy",
+  icon: "mussy",
+  initialOpenGroupIndex: -1,
+  children: [
+    "/article/mussy/01-202112",
+  ],
+};
